403980463d7a67aa5ccdcc0ec45835e395abb554
Javokhir <javokhir.bozorov@gmail.com>
2023-02-02 12:54:41 +0000

    udate

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -8,11 +8,12 @@ export const Home = () =>{
     return(
         <>
         <Header title={"TO-DO"}/>
+        <Box sx={{color:"white"}} component="h2">Double click the task to EDIT</Box>
             <Box sx={{
                 margin:'auto',
                 width:'80%',
                 minHeight:'30vh',
-                marginTop:'30%',
+                marginTop:'10%',
                 borderRadius:'20px',
                 backgroundColor:'white',
                 paddingTop:'15px',
